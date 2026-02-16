10b7c44eddc886598ce78bd901ce6f3001d62c2a
Eugene Blikh <bigbes@gmail.com>
2026-02-17 02:08:40 +0300

    Add lua-amalgamate@0.1.2

---
+++ Formula/lua-amalgamate@0.1.2.rb
@@ -0,0 +1,25 @@
+class LuaAmalgamateAT012 < Formula
+  desc "Lua amalgamator"
+  homepage "https://github.com/bigbes/lua-amalgamate"
+  url "https://github.com/bigbes/lua-amalgamate.git",
+    tag: "v0.1.2",
+    revision: "e89f7ec4db409192f755e08b3ff632e5547bff97"
+  license "MIT"
+  head "https://github.com/bigbes/lua-amalgamate.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    build_date = Time.now.strftime("%Y-%m-%d-%H:%M:%S")
+    build_commit = Utils.git_head&.slice(0, 7) || "unknown"
+
+    ldflags = "-s -w -X main.version=#{version} -X main.commit=#{build_commit} -X main.date=#{build_date}"
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/lua-amalgamate"
+  end
+
+  test do
+    output = shell_output("#{bin}/lua-amalgamate --version")
+    assert_match(/^lua-amalgamate version #{version}/, output)
+  end
+end
